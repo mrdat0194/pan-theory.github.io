2e17dab193ca01cd9c09b29ababbc72a5f37931d
petern <petermrdat09@gmail.com>
2021-10-08 11:34:06 +0700

    chisq statistics

--- Bayesian/abtesting/Excercise.py
+++ Bayesian/abtesting/Excercise.py
@@ -1,2 +1,12 @@
 # https://docs.google.com/spreadsheets/d/1-Kip2_MicfV0ebRZX8N5vhj3b-8RxuQp/edit#gid=1415247164
 # https://docs.google.com/spreadsheets/d/1CpRMgzMfWxmvRASPkfHOd4XBQDNdaW2Q_W1-GitubtU/edit#gid=138966392
+
+# ztest
+# https://colab.research.google.com/drive/1hu-vJLeUHTQEn8fytWWRMiI2z-mtpIvS?usp=sharinghttps://colab.research.google.com/drive/1hu-vJLeUHTQEn8fytWWRMiI2z-mtpIvS?usp=sharing
+
+# https://docs.pymc.io/Gaussian_Processes.html
+# https://en.wikipedia.org/wiki/Gaussian_process
+
+# https://www.stats.ox.ac.uk/~teh/research/npbayes/Teh2010a.pdf
+# https://www.stats.ox.ac.uk/~teh/research/npbayes/Teh2010a.pdf
+
